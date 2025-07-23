b30bf5c2c01ebd10f2556ccdd933543e7247a316
Shivang Yadav <yadavshivang2108@gmail.com>
2025-07-23 18:12:00 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,9 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: "https://brandify-ai-azure.vercel.app"
+}));
 app.use(express.json());
 
 // Routes
